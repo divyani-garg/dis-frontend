dd255ca1841f8e4354838cc1761a021007765b81
Manas Jain <manasjain@gmail.com>
2019-02-15 18:15:06 +0530

    Common Module Added

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CommonRoutingModule } from './common-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CommonRoutingModule
+  ],
+  declarations: []
+})
+export class CommonModule { }

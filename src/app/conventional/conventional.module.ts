ad5c079c175416edd658c7bca419b382ab9e50c5
Manas Jain <manasjain@gmail.com>
2019-02-15 18:43:33 +0530

    Added Conventional Module

---
+++ src/app/conventional/conventional.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConventionalRoutingModule } from './conventional-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ConventionalRoutingModule
+  ],
+  declarations: []
+})
+export class ConventionalModule { }

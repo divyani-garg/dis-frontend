b3ec194837b6f6c799e2c228299321ada8341597
Singhal-vishal <vishalsinghalcool16@gmail.com>
2019-02-23 18:52:11 +0530

    profile service

--- src/app/API_Service/profile.service.ts
+++ src/app/API_Service/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private baseUrl="";
-  constructor() { }
+
+  
+  private baseUrl="http://localhost:8081";
+  constructor(private http: HttpClient) { }
+  getProfileInfo():Observable<any>{
+    return this.http.get(`${this.baseUrl}/staffBasicProfile`);
+  }
 }
--- src/app/conventional/profile/profile.component.ts
+++ src/app/conventional/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProfileService } from 'src/app/API_Service/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  @Input() profileData: any;
+  staffBasicProfile:any;
+  userId:string="196";
+  
+  constructor(private profile:ProfileService) { }
 
   ngOnInit() {
+    this.profileData=this.profile.getProfileInfo()
+    .subscribe(
+      data=>{
+          if(this.checkAuthenticateUser(data.employeeId)){
+            this.staffBasicProfile=data;
+            }
+               console.log(this.staffBasicProfile);
+      },
+     )
+     //console.log(this.profileData);
+     
   }
-  
+    checkAuthenticateUser(id):boolean
+     {
+       if(id===this.userId)
+       {
+         return true;
+       }
+       else
+       {
+          return false;
+       }
+     } 
+    
 }

318b2541955e034043cc09fb4dcee7ac3d27be7a
Divyani Garg <divyanigarg09@gmail.com>
2019-06-06 13:41:37 +0530

    student faculty merge

--- src/app/faculty/faculty-routing.module.ts
+++ src/app/faculty/faculty-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from '../hod/student/student.component';
 import { FacultyComponent } from './faculty/faculty.component';
 import { AttendanceComponent } from './attendance/attendance.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -13,7 +14,6 @@ import { MydutiesComponent } from '../myduties/myduties/myduties.component';
 import { InfrastructureModule } from '../infrastructure/infrastructure.module';
 import { InfrastructureOutletComponent } from '../infrastructure/infrastructure-outlet/infrastructure-outlet.component';
 import { ProfileComponent } from '../conventional/profile/profile.component';
-import { StudentComponent } from '../hod/student/student.component';
 import { HodModule } from '../hod/hod.module';
 import { TimetableComponent } from '../hod/timetable/timetable.component';
 
